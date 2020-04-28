32d5817b8965edc9e81bfc96f846f6f2c51f3c80
Andrew Steward <andrewsteward@quickenloans.com>
2020-04-28 08:42:03 -0400

    LAB 16.1 CSV

---
+++ LAB_11.2_CoffeeShop/LAB_11.2_CoffeeShop/Controllers/RegistrationController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LAB_11._2_CoffeeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LAB_11._2_CoffeeShop.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("RegistrationIndex");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult RegistrationConfirmation(MemberRegistration member)
+        {
+
+            return View(member);
+
+            //if (ModelState.IsValid)
+            //{
+            //    return View(member);
+            //}
+            //else
+            //{
+            //    ViewData["errorMsg"] = "Your form had errors. Please correct and re-submit.";
+            //    return View("RegistrationIndex", member);
+            //}
+        }
+    }
+}
